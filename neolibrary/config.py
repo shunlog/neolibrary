fbd344c34d76d597d676a578cca9ac6d9d4f61a6
Artiom White <48106195+Kyle-is-online@users.noreply.github.com>
2020-04-09 20:12:50 +0300

    creating heroku app

---
+++ neolibrary/config.py
@@ -0,0 +1,5 @@
+class Config:
+        SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+        DB_PASSWORD = 'adminadmin'
+        book_covers='static/book_covers/'
+        profile_pics='static/profile_pics/'
