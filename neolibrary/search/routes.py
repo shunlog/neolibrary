82f589a79d6192360b81ac4fbb5ad6d50bed9d8e
Artiom White <48106195+Kyle-is-online@users.noreply.github.com>
2020-04-09 14:40:28 +0300

    better search

--- neolibrary/search/routes.py
+++ neolibrary/search/routes.py
@@ -13,33 +13,33 @@ def search():
     search = SearchForm(request.form)
 
     if request.method == 'POST':
-        search_string = search.data['search']
+        search_str = search.data['search']
 
         if search.data['select'] == "Book":
             if search.data['search'] == '':
                 books = Book().match(graph)
             else:
-                books = Book().match(graph, search_string)
+                books = Book().match(graph).where("_.title =~ '(?i).*{0}.*'".format(search_str))
             if not books:
                 flash('No results found!')
-            return render_template('search.html', books=books, form=search, image_folder=book_covers)
+            return render_template('search.html', books=books, form=search, image_folder=book_covers, title="Search")
 
         elif search.data['select'] == "Author":
             if search.data['search'] == '':
                 authors = Author().match(graph)
             else:
-                authors = Author().match(graph, search_string)
+                authors = Author().match(graph).where("_.name =~ '(?i).*{0}.*'".format(search_str))
             if not authors:
                 flash('No results found!')
-            return render_template('search.html', authors=authors, form=search, image_folder=book_covers)
+            return render_template('search.html', authors=authors, form=search, image_folder=book_covers, title="Search")
 
         elif search.data['select'] == "Tag":
             if search.data['search'] == '':
                 tags = Tag().match(graph)
                 print(search.data['select'])
             else:
-                tags = Tag().match(graph, search_string)
+                tags = Tag().match(graph).where("_.name =~ '(?i).*{0}.*'".format(search_str))
             if not tags:
                 flash('No results found!')
-            return render_template('search.html', tags=tags, form=search, image_folder=book_covers)
-    return render_template('search.html', form=search)
+            return render_template('search.html', tags=tags, form=search, image_folder=book_covers, title="Search")
+    return render_template('search.html', form=search, title="Search")
