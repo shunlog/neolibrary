6f69402aff9c01329f9697fb57039dc6ad981d71
Artiom White <48106195+Kyle-is-online@users.noreply.github.com>
2020-04-14 14:54:57 +0300

    example_config no more needed, since all the secrets are kept in environment variables

--- neolibrary/example_config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-        app.config['SECRET_KEY'] = 'random_string'
-        graph = Graph(password="your_password")
-        book_covers='static/book_covers/'
-        profile_pics='static/profile_pics/'
