8b86e274c122c4febc38586395d7145b0c6a2cbd
Artiom White <48106195+Kyle-is-online@users.noreply.github.com>
2020-04-22 16:03:10 +0300

    added select dropdown on search page

--- neolibrary/search/forms.py
+++ neolibrary/search/forms.py
@@ -3,4 +3,8 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 
 class SearchForm(FlaskForm):
+    choices = [('All', 'All'),
+               ('Author', 'Author'),
+               ('Tag', 'Tag')]
+    select = SelectField('Search:', choices=choices)
     search = StringField('search', id='form-search')
--- neolibrary/search/routes.py
+++ neolibrary/search/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request, abort, Blueprint, jsonify
 from flask_login import current_user, login_required
+from werkzeug.datastructures import MultiDict
 from neolibrary import graph, book_covers
 from neolibrary.models import Book, Author, User, Tag
 from neolibrary.search.forms import SearchForm
@@ -11,46 +12,50 @@ n_limit = Book.n_limit
 
 @search_bl.route("/search", methods=['GET', 'POST'])
 def search():
-
-    search = SearchForm(request.form)
-    page = request.args.get('page',1, type=int)
     search_str = request.args.get('search', type=str)
-    # match romanian chars
+    select_str = request.args.get('select')
+    search = SearchForm()
+    search.search.data = search_str
+    search.select.data = select_str
 
-    if search_str and search_str != '':
+    if search.data['select'] == "All":
+        page = request.args.get('page',1, type=int)
+        # match romanian chars
 
-        search_regexp = str_to_regexp(search_str)
+        if search_str and search_str != '':
 
-        query = "match (a:Author)-->(b:Book)\
-            where b.title=~$search_str \
-            return b union\
-            match (b)<--(t:Tag)\
-            where t.name=~$search_str \
-            return b union\
-            match (b)<--(a:Author)\
-            where a.name=~$search_str \
-            return distinct b"
+            search_regexp = str_to_regexp(search_str)
 
-        dt = graph.run(query,search_str=search_regexp)
-        books = [Book.wrap(node[0]) for node in dt]
+            query = "match (a:Author)-->(b:Book)\
+                where b.title=~$search_str \
+                return b union\
+                match (b)<--(t:Tag)\
+                where t.name=~$search_str \
+                return b union\
+                match (b)<--(a:Author)\
+                where a.name=~$search_str \
+                return distinct b"
 
-        count = len(books)
-        pages = count // n_limit if count % n_limit == 0 else count // n_limit + 1
-        if page > pages:
-            page = pages
-        elif page < 1:
-            page = 1
-        page_ls = iter_pages(pages, page, 2, 1, 1, 3)
+            dt = graph.run(query,search_str=search_regexp)
+            books = [Book.wrap(node[0]) for node in dt]
 
-        n_skip = abs(n_limit * (page - 1))
-        books = books[n_skip:n_skip+n_limit]
+            count = len(books)
+            pages = count // n_limit if count % n_limit == 0 else count // n_limit + 1
+            if page > pages:
+                page = pages
+            elif page < 1:
+                page = 1
+            page_ls = iter_pages(pages, page, 2, 1, 1, 3)
 
-        if books:
-            return render_template('search.html', books=books, form=search, book_covers=book_covers,
-                            title="Search", page_ls=page_ls,
-                            search=search_str, current_page=page)
-        else:
-            flash("Couldn't find any books","danger")
+            n_skip = abs(n_limit * (page - 1))
+            books = books[n_skip:n_skip+n_limit]
+
+            if books:
+                return render_template('search.html', books=books, form=search, book_covers=book_covers,
+                                title="Search", page_ls=page_ls,
+                                search=search_str, current_page=page)
+            else:
+                flash("Couldn't find any books","danger")
 
     query = "match(b:Book) optional match (b)--(u:User) return b, count(u)\
             order by count(u) desc, b.title limit $limit"
