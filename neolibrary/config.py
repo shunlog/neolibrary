d65791ebf782d2b5655fb2ce3db3897f463d7476
Artiom White <48106195+Kyle-is-online@users.noreply.github.com>
2020-05-05 17:51:12 +0300

    add comments to config file

--- neolibrary/config.py
+++ neolibrary/config.py
@@ -1,16 +1,28 @@
 import os
 
 class Config:
+        # graphene database (is set automatically on heroku)
         GRAPHENEDB_URL = os.getenv("GRAPHENEDB_BOLT_URL")
         GRAPHENEDB_USER = os.getenv("GRAPHENEDB_BOLT_USER")
         GRAPHENEDB_PASSWORD = os.getenv('GRAPHENEDB_BOLT_PASSWORD')
+
+        # local database
         DB_PASSWORD = os.getenv('DB_PASSWORD')
         SECRET_KEY = os.getenv('SECRET_KEY')
-        BOOK_COVERS = os.getenv('BOOK_COVERS')
-        BOOK_COVERS_PATH = os.getenv('BOOK_COVERS_PATH')
+
+        # folder path from static that stores pics
         PROFILE_PICS = os.getenv('PROFILE_PICS')
+        BOOK_COVERS = os.getenv('BOOK_COVERS')
+
+        # link to the site
+        BOOK_COVERS_PATH = os.getenv('BOOK_COVERS_PATH')
         PROFILE_PICS_PATH = os.getenv('PROFILE_PICS_PATH')
+
+        # ftp to server that hosts pics
         FTP_USER = os.getenv("FTP_USER")
         FTP_PASSWORD = os.getenv("FTP_PASSWORD")
         HOST_IP = os.getenv("HOST_IP")
+
+        # directory path that contains the folders indicated
+        # in book_COVERS and PROFILE_PICS
         HOST_DIR = os.getenv("HOST_DIR")
