f6cab6d924845e97974b6ff2e13c11a2466044a4
Artiom White <48106195+Kyle-is-online@users.noreply.github.com>
2021-04-02 14:22:58 +0300

    Refactor home page

--- neolibrary/main/routes.py
+++ neolibrary/main/routes.py
@@ -14,17 +14,18 @@ def home():
     global book_covers_path
     if not book_covers_path:
         book_covers_path = url_for('static', filename=book_covers)
+
+    books_recommended = {}
     if current_user.is_authenticated:
         sidebar = init_sidebar(current_user)
 
-        books_recommended = {}
         query = "match (b:Book)<-[:LIKED]-(:User)-[:LIKED]->(c:Book)<-[:LIKED]-(u:User)\
                     where u.username=$username and not (u)-->(b)\
                     return b, count(c)\
                     order by count(c) desc\
                     limit $limit"
         dt = graph.run(query, username=current_user.username, limit=Config.BOOKS_LIMIT_HOME)
-        books_recommended["users"] = [Book.wrap(node) for node,c in dt]
+        books_recommended["Similar users:"] = [Book.wrap(node) for node,c in dt]
 
         query = "match (b:Book)<-[:TAGS]-()-[:TAGS]->(c:Book)<-[:LIKED]-(u:User)\
                     where u.username=$username and not (u)-->(b)\
@@ -32,7 +33,7 @@ def home():
                     order by count(c) desc\
                     limit $limit"
         dt = graph.run(query, username=current_user.username, limit=Config.BOOKS_LIMIT_HOME)
-        books_recommended["tags"] = [Book.wrap(node) for node,c in dt]
+        books_recommended["Similar tags:"] = [Book.wrap(node) for node,c in dt]
 
         query = "match (b:Book)<-[:WROTE]-()-[:WROTE]->(c:Book)<-[:LIKED]-(u:User)\
                     where u.username=$username and not (u)-->(b)\
@@ -40,17 +41,21 @@ def home():
                     order by count(c) desc\
                     limit $limit"
         dt = graph.run(query, username=current_user.username, limit=Config.BOOKS_LIMIT_HOME)
-        books_recommended["authors"] = [Book.wrap(node) for node,c in dt]
+        books_recommended["Similar authors:"] = [Book.wrap(node) for node,c in dt]
 
 
         return render_template('home.html', title='Home',sidebar=sidebar,
-                                books_recommended=books_recommended, book_covers_path=book_covers_path)
+                                books_recommended=books_recommended,
+                               book_covers_path=book_covers_path)
     else:
         sidebar = init_sidebar(None)
         query = "match(b:Book) optional match (b)--(u:User) return b, count(u)\
                 order by count(u) desc, b.title limit $limit"
         dt = graph.run(query,limit=Config.BOOKS_LIMIT_HOME)
-        books = [Book.wrap(node) for node,c in dt]
+        books_recommended["Most popular:"] = [Book.wrap(node) for node,c in dt]
+        for key in books_recommended:
+            print(key)
 
         return render_template('home.html', title='Home',sidebar=sidebar,
-                                books=books, book_covers_path=book_covers_path)
+                                books_recommended=books_recommended,
+                               book_covers_path=book_covers_path)
